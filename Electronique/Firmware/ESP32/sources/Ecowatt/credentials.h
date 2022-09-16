4a76efad77a3ffff6c450e765b3ab44fde3664af
battosai30 <batto@hotmail.fr>
2022-09-16 15:08:26 +0200

    Update credentials.h

--- Electronique/Firmware/ESP32/sources/Ecowatt/credentials.h
+++ Electronique/Firmware/ESP32/sources/Ecowatt/credentials.h
@@ -1,8 +1,12 @@
 //RTE base64 API Access key
-char api_key[] = "XXXXXXXtrgetrgXXXXXXXXXXX";
+char api_key[] = "YWJlZmMzYjItMjQ0MC00NWUzLThmMjgtNzA3Yjg1NzM4YjRhOjU1NWMzMDYxLTBmNDctNGRmOS05NDZlLTYzMmE1Njk0MTY3ZA==";
 
 
 // Wifi credentials
-
+/*
 const char* wifiSSID = "XXXXXXXXXXX";
 const char* wifiPASSWORD = "XXXXXXXXXXXXXXXXX";
+*/
+
+const char* wifiSSID = "LABSud";
+const char* wifiPASSWORD = "Open Hardware & Open Source";
